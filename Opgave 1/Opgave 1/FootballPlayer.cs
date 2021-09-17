ce99c4a6f05dfcd631eec56c6fe52d9bc4d7c6a2
Oliver-Rasmussen <oliver.rasmussen@outlook.dk>
2021-09-17 15:32:56 +0200

    Opgave 1
    Opgave 1 som indeholder FootballPlayer klassen, samt unit testing

---
+++ Opgave 1/Opgave 1/FootballPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Opgave_1
+{
+    public class FootballPlayer
+    {
+        private int _ID;
+        private string _Name;
+        private int _Price;
+        private int _ShirtNumber;
+
+        public int ID
+        {
+            get => _ID;
+            set
+            {
+                _ID = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (value.Length < 4) { throw new ArgumentOutOfRangeException("Name must be 4 char min."); }
+                    _Name = value;
+            }
+        }
+
+        public int Price
+        {
+            get => _Price;
+            set
+            {
+                if(value <= 0) { throw new ArgumentOutOfRangeException("Positive numbers only"); }
+                _Price = value;
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _ShirtNumber;
+            set
+            {
+                if(value < 0 || value > 100) { throw new ArgumentOutOfRangeException("Must be between 1 and 100"); }
+                _ShirtNumber = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{_ID} - {_Name}: Worth: {_Price} - Number: {_ShirtNumber}";
+        }
+    }
+}
---
+++ Opgave 1/Opgave 1Tests/FootballPlayerTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opgave_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave_1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        [TestMethod()]
+        public void NameExceptionTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { player.Name = "Bo"; });
+        }
+
+        [TestMethod()]
+        public void NameChangeTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+            player.Name = "Karl";
+
+            Assert.AreEqual(player.Name, "Karl");
+        }
+
+        [TestMethod()]
+        public void PriceExceptionTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { player.Price = 0; });
+        }
+
+        [TestMethod()]
+        public void PriceChangeTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+            player.Price = 500;
+
+            Assert.AreEqual(player.Price, 500);
+        }
+
+        [TestMethod()]
+        public void ShirtNumberExceptionTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => { player.ShirtNumber = 101; });
+        }
+
+        [TestMethod()]
+        public void ShirtNumberChangeTest()
+        {
+            FootballPlayer player = new FootballPlayer();
+            player.ShirtNumber = 99;
+
+            Assert.AreEqual(player.ShirtNumber, 99);
+        }
+    }
+}
