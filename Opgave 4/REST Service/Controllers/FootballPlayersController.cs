eb7ff54652584d462c79a40374708783b19b0150
Oliver-Rasmussen <oliver.rasmussen@outlook.dk>
2021-09-18 12:51:03 +0200

    Opgave 4
    REST Service + Rettelse i Opgave 2 HTML

---
+++ Opgave 4/REST Service/Controllers/FootballPlayersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using REST_Service.Managers;
+using Opgave_1;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace REST_Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FootballPlayersController : ControllerBase
+    {
+        private readonly FootballPlayersManager _manager = new FootballPlayersManager();
+
+        // GET: api/<FootballPlayersController>
+        [HttpGet]
+        public IEnumerable<FootballPlayer> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<FootballPlayersController>/5
+        [HttpGet("{id}")]
+        public FootballPlayer Get(int id)
+        {
+            return _manager.GetById(id);
+        }
+
+        // POST api/<FootballPlayersController>
+        [HttpPost]
+        public IEnumerable<FootballPlayer> Post([FromBody] FootballPlayer value)
+        {
+            _manager.Add(value);
+            return _manager.GetAll();
+        }
+
+        // PUT api/<FootballPlayersController>/5
+        [HttpPut("{id}")]
+        public IEnumerable<FootballPlayer> Put(int id, [FromBody] FootballPlayer value)
+        {
+            _manager.Update(id, value);
+            return _manager.GetAll();
+        }
+
+        // DELETE api/<FootballPlayersController>/5
+        [HttpDelete("{id}")]
+        public IEnumerable<FootballPlayer> Delete(int id)
+        {
+            _manager.Delete(id);
+            return _manager.GetAll();
+        }
+    }
+}
---
+++ Opgave 4/REST Service/Managers/FootballPlayersManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Opgave_1;
+
+namespace REST_Service.Managers
+{
+    public class FootballPlayersManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<FootballPlayer> Data = new List<FootballPlayer>
+        {
+            new FootballPlayer {ID = _nextId++, Name = "Anders Bjørn", Price = 55000, ShirtNumber = 54},
+            new FootballPlayer {ID=_nextId++, Name = "Erik Hansen", Price = 69000, ShirtNumber = 33}
+        };
+
+        public List<FootballPlayer> GetAll()
+        {
+            return new List<FootballPlayer>(Data);
+        }
+
+        public FootballPlayer GetById(int id)
+        {
+            return Data.Find(player => player.ID == id);
+        }
+
+        public FootballPlayer Add(FootballPlayer player)
+        {
+            player.ID = _nextId++;
+            Data.Add(player);
+            return player;
+        }
+
+        public FootballPlayer Delete(int id)
+        {
+            FootballPlayer player = Data.Find(player1 => player1.ID == id);
+            if (player == null) return null;
+            Data.Remove(player);
+            return player;
+        }
+
+        public FootballPlayer Update(int id, FootballPlayer updates)
+        {
+            FootballPlayer player = Data.Find(book1 => book1.ID == id);
+            if (player == null) return null;
+            player.Name = updates.Name;
+            player.Price = updates.Price;
+            return player;
+        }
+    }
+}
